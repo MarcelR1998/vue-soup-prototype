a47bf103c4ddfe710f2563f2db339389807b060d
MarcelR1998 <marcelrossi@hotmail.com>
2020-10-16 13:25:26 +0200

    Created display of popular soups

--- src/components/Home/SoupList.vue
+++ src/components/Home/SoupList.vue
@@ -2,6 +2,16 @@
   <div class="soupList">
     <div class="widthContainer">
       <h2>Popular</h2>
+      <div class="popularSoups">
+        <div v-for="soup in soupData" :key="'popular' + soup.id">
+          <PopularSoupCard
+            :name="soup.name"
+            :price="soup.price"
+            :imageUrl="soup.imageUrl"
+            :id="soup.id"
+          />
+        </div>
+      </div>
 
       <h2>All our soups</h2>
       <!-- Renders a list of all soups -->
@@ -19,10 +29,11 @@
 
 <script>
 import SoupCard from "./SoupCard";
+import PopularSoupCard from "./popularSoupCard";
 
 export default {
   name: "SoupList",
-  components: { SoupCard },
+  components: { SoupCard, PopularSoupCard },
   computed: {
     soupData() {
       return this.$store.state.soupData;
@@ -35,5 +46,10 @@ export default {
 .soupList {
   padding: 8px;
   background-color: rgb(250, 250, 250);
+}
+.popularSoups {
+  display: flex;
+  flex-direction: row;
+  overflow-x: auto;
 }
 </style>
---
+++ src/components/Home/popularSoupCard.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="popularSoupCard">
+    <router-link :to="'/soup/' + id">
+      <div class="imageContainer">
+        <img :src="imageUrl" alt />
+      </div>
+      <h4>{{ name }}</h4>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PopularSoupCard",
+  props: ["name", "price", "imageUrl", "id"],
+};
+</script>
+
+<style scoped>
+.popularSoupCard {
+  margin-right: 12px;
+  border-radius: 12px;
+  transition: 0.5s;
+  width: 128px;
+}
+.popularSoupCard:hover img {
+  transform: scale(1.1);
+}
+.imageContainer {
+  overflow: hidden;
+  border-radius: 12px 12px;
+}
+img {
+  width: 128px;
+  border-radius: 12px 12px;
+  object-fit: cover;
+  height: 128px;
+  transition: 0.5s;
+  display: block;
+}
+.cardInfo {
+  display: flex;
+  justify-content: space-between;
+}
+a {
+  color: black;
+  text-decoration: none;
+}
+h4 {
+  margin: 8px 0 0;
+}
+</style>
