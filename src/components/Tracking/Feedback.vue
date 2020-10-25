06e860e8eaef634d3a26094ce5119ca8a6a81b44
MarcelR1998 <marcelrossi@hotmail.com>
2020-10-25 16:36:40 +0100

    Added fade in on feedback form in tracking and animated stars on hover

--- src/components/Tracking/Feedback.vue
+++ src/components/Tracking/Feedback.vue
@@ -1,30 +1,40 @@
 <template>
-  <div>
+  <div v-bind:class="{ fadeIn: fadeIn }" class="feedBack">
     <h3>How was your experience?</h3>
     <div id="starBar">
       <i
-        v-bind:class="{ starred: stars > 0 }"
+        v-bind:class="{ starred: stars > 0, hoverStarred: hoverStars > 0 }"
         @click="setStar(1)"
+        @mouseenter="hoverEnterStar(1)"
+        @mouseleave="hoverLeaveStar"
         class="fas fa-star"
       ></i>
       <i
-        v-bind:class="{ starred: stars > 1 }"
+        v-bind:class="{ starred: stars > 1, hoverStarred: hoverStars > 1 }"
         @click="setStar(2)"
+        @mouseenter="hoverEnterStar(2)"
+        @mouseleave="hoverLeaveStar"
         class="fas fa-star"
       ></i>
       <i
-        v-bind:class="{ starred: stars > 2 }"
+        v-bind:class="{ starred: stars > 2, hoverStarred: hoverStars > 2 }"
         @click="setStar(3)"
+        @mouseenter="hoverEnterStar(3)"
+        @mouseleave="hoverLeaveStar"
         class="fas fa-star"
       ></i>
       <i
-        v-bind:class="{ starred: stars > 3 }"
+        v-bind:class="{ starred: stars > 3, hoverStarred: hoverStars > 3 }"
         @click="setStar(4)"
+        @mouseenter="hoverEnterStar(4)"
+        @mouseleave="hoverLeaveStar"
         class="fas fa-star"
       ></i>
       <i
-        v-bind:class="{ starred: stars > 4 }"
+        v-bind:class="{ starred: stars > 4, hoverStarred: hoverStars > 4 }"
         @click="setStar(5)"
+        @mouseenter="hoverEnterStar(5)"
+        @mouseleave="hoverLeaveStar"
         class="fas fa-star"
       ></i>
     </div>
@@ -43,26 +53,45 @@ export default {
   data() {
     return {
       stars: 0,
+      hoverStars: 0,
+      fadeIn: false,
     };
   },
   methods: {
     setStar(item) {
       this.stars = item;
     },
+    hoverEnterStar(item) {
+      this.hoverStars = item;
+    },
+    hoverLeaveStar() {
+      this.hoverStars = 0;
+    },
+  },
+  created() {
+    setTimeout(() => {
+      this.fadeIn = true;
+    }, 250);
   },
 };
 </script>
 
 <style scoped>
+.feedBack {
+  opacity: 0;
+  transform: translateY(32px);
+  transition: 1s;
+}
+.fadeIn {
+  opacity: 1;
+  transform: translateY(0);
+}
 h3 {
   text-align: center;
 }
 #starBar {
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
+  width: fit-content;
   margin: auto;
-  width: 160px;
 }
 .fa-star {
   color: #c4c4c4;
@@ -71,6 +100,9 @@ h3 {
 }
 .starred {
   color: #f3db00;
+}
+.hoverStarred {
+  color: #f3db0080;
 }
 #improvementButtons {
   display: flex;
